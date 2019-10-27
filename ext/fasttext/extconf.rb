249447d5d8213ffc6e6190c662e95de8f0c310bf
Andrew Kane <andrew@chartkick.com>
2019-10-27 13:03:12 -0700

    Cleaner extconf

--- ext/fasttext/extconf.rb
+++ ext/fasttext/extconf.rb
@@ -5,11 +5,11 @@ abort "Missing stdc++" unless have_library("stdc++")
 # TODO use -std=c++14 when available
 $CXXFLAGS << " -pthread -std=c++11 -funroll-loops -O3 -march=native"
 
-src1 = File.expand_path(".", __dir__)
-src2 = File.expand_path("../../vendor/fastText/src", __dir__)
+ext = File.expand_path(".", __dir__)
+fasttext = File.expand_path("../../vendor/fastText/src", __dir__)
 
-$srcs = Dir["{#{src1},#{src2}}/*.{cc,cpp}"]
-$INCFLAGS << " -I$(srcdir)/../../vendor/fastText/src"
-$VPATH << "$(srcdir)/../../vendor/fastText/src"
+$srcs = Dir["{#{ext},#{fasttext}}/*.{cc,cpp}"]
+$INCFLAGS << " -I#{fasttext}"
+$VPATH << fasttext
 
 create_makefile("fasttext/ext")
