219af953784f28e56821af3b09142fe6b51bcec4
Andrew Kane <andrew@chartkick.com>
2020-02-17 14:58:38 -0800

    Min flags

--- ext/fasttext/extconf.rb
+++ ext/fasttext/extconf.rb
@@ -3,7 +3,8 @@ require "mkmf-rice"
 # abort "Missing stdc++" unless have_library("stdc++")
 
 # TODO use -std=c++14 when available
-$CXXFLAGS << " -pthread -std=c++11 -funroll-loops -O3 -march=native"
+# $CXXFLAGS << " -pthread -std=c++11 -funroll-loops -O3 -march=native"
+$CXXFLAGS << " -std=c++11"
 
 ext = File.expand_path(".", __dir__)
 fasttext = File.expand_path("../../vendor/fastText/src", __dir__)
